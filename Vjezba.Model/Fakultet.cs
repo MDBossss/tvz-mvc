571198e340ce2b4442e005f9513e87b0869ec12f
MDBossss <ferrari020203@gmail.com>
2024-03-17 10:12:16 +0100

    Zadatak 1.3 - DohvatiStudente funkcija

--- Vjezba.Model/Fakultet.cs
+++ Vjezba.Model/Fakultet.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace Vjezba.Model;
 
@@ -14,22 +17,45 @@ public class Fakultet
 
     public int KolikoStudenata()
     {
-        return Osobe.Count(osoba => osoba is Student)
+        return Osobe.Count(osoba => osoba is Student);
     }
 
     public Student DohvatiStudenta(string JMBAG)
     {
-        Osobe.ofType<Student>().FirstOrDefault(student => student.JMBAG == JMBAG)
+        return Osobe.OfType<Student>().FirstOrDefault(student => student.JMBAG == JMBAG);
     }
 
     public IEnumerable<Profesor> DohvatiProfesore()
     {
-        // Filter the profesors from the aray
+        // Filter the profesors from the array
         var profesori = Osobe.Where(Osoba => Osoba is Profesor).Cast<Profesor>();
 
         // Order by DatumIzbora
         profesori = profesori.OrderBy(profesori => profesori.DatumIzbora);
 
         return profesori;
+    }
+
+    public IEnumerable<Student> DohvatiStudente91()
+    {
+        // Filter the students from the array which are born after year 1991
+        return Osobe.Where(Osoba => Osoba is Student).Cast<Student>().Where(student => student.DatumRodenja.Year > 1991);
+    }
+
+    public IEnumerable<Student> DohvatiStudente91NoLinq()
+    {
+        var studenti = new List<Student>();
+
+        foreach (var osoba in Osobe)
+        {
+            if(osoba is Student && osoba.DatumRodenja.Year > 1991)
+            {
+                studenti.Add(osoba as Student);
+            }
+        }
+
+        return studenti;
+
+
     }
 }
